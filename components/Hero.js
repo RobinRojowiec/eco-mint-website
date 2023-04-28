1033bc833a0be3293155c6d817b52468cac5cae6
Robin Rojowiec <robin@meta.link>
2023-04-28 17:15:15 +0200

    updated hero section

--- components/Hero.js
+++ components/Hero.js
@@ -127,11 +127,10 @@ export default function Hero() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Data to enrich your online business
+             Ecomint
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-300">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+              Sustainable investments for everyone!
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
