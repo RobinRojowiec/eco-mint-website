0d06808b1d34d7c1982365d9a20ee87bc5c1aa26
Nick <121521386+nick-urb@users.noreply.github.com>
2023-06-20 23:30:40 +0200

    Added intro-main section

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Hero from '@components/Hero'
+import IntroMain from '@components/Intro-main'
 import Newsletter from '@components/Newsletter'
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Hero />
+      <IntroMain />
       <Newsletter />
       <Footer />
     </div>
